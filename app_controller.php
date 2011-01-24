dd0912cc3ccc142a4c893de7bed5b3bf535c22a2
Niles <neterslandreau@gmail.com>
2011-01-24 13:52:35 -0500

    adding initial modifications for the app controller.

--- app_controller.php
+++ app_controller.php
@@ -36,4 +36,149 @@ class AppController extends Controller {
 		'Session',
 		'DebugKit.Toolbar'
 	);
+
+/**
+ * A list of the resources that have been included in access control.
+ * This is used by the Nssrbac plugin
+ *
+ * @var array
+ * @access public
+ */
+	public $protectedResources = array();
+
+/**
+ * permissionsMap
+ *
+ * @var array
+ * @access public
+ */
+	public $permissionsMap = array(
+		'create' => array('add'),
+		'read' => array('index', 'view'),
+		'update' => array('edit'),
+		'delete' => array('delete'),
+	);
+
+/**
+ * beforeFilter Callback
+ *
+ * @return void
+ * @access public
+ */
+	public function beforeFilter() {
+		$admin = Configure::read('Routing.admin');
+
+		// added for the Nssrbac plugin
+		$authId = $this->Auth->user('id');
+		if ($authId && !is_array($this->Session->read('Auth.Role'))) {
+			$this->setRoles();
+		}
+
+		// required for the Nssrbac plugin
+		$this->Auth->authorize = 'controller';
+
+		// required to populate $this->protectedResources array
+		$this->setResources();
+
+		$this->Auth->loginAction = array('controller' => 'users', 'action' => 'login', 'prefix' => $admin, $admin => false, 'plugin' => null);
+		$this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'login', 'prefix' => $admin, $admin => false, 'plugin' => null);
+		$this->Auth->loginError = __('Invalid username / password combination.  Please try again', true);
+		$this->Auth->loginRedirect = array('controller' => 'articles', 'action' => 'index', 'prefix' => $admin, $admin => false, 'plugin' => null);
+	}
+
+/**
+ * isAuthorized callback; does the majority of the heavy lifting for the Nssrbac plugin
+ * Required for Auth->authorize('controller')
+ *
+ * @return boolean
+ * @access public
+ */
+	public function isAuthorized() {
+		if (in_array($this->name, $this->protectedResources)) {
+			$permissions = $this->getPermissions($this->name);
+			if (in_array($this->action, $permissions)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+
+/**
+ * Get a list of all the resources that are access controlled and populate $this->protectedResources
+ *
+ * @return boolean
+ * @access public
+ */
+	public function setResources() {
+		$Resource = ClassRegistry::init('Nssrbac.Resource');
+		$resources = $Resource->find('all');
+		foreach($resources as $resource) {
+			$this->protectedResources[] = Inflector::camelize(Inflector::pluralize($resource['Resource']['name']));
+		}
+		return true;
+	}
+
+/**
+ * Combines the user roles into the session variable
+ *
+ * @return boolean
+ * @access public
+ */
+	public function setRoles() {
+		$this->User = ClassRegistry::init('User');
+		$roles = $this->User->find('first', array(
+			'conditions' => array(
+				'User.id' => $this->Auth->user('id'),
+			),
+		));
+		$auth = $this->Session->read('Auth');
+		$roleAuth = am($roles, $auth);
+		$this->Session->write('Auth', $roleAuth);
+		return true;
+	}
+
+/**
+ * Get a list of authorized methods for a particular user/role to a resource
+ *
+ * @param string $resourceName
+ * @return array $permittedMethods
+ * @access public
+ */
+	public function getPermissions($resourceName = null) {
+		$permissionNames = array();
+		$permittedMethods = array();
+		$Resource = ClassRegistry::init('Nssrbac.Resource');
+		$Permission = ClassRegistry::init('Nssrbac.Permission');
+		$roles = $this->Session->read('Auth');
+		$myRoleIds = Set::Extract('/Role/id', $roles);
+		$permissions = $Permission->find('all', array(
+			'conditions' => array(
+				'Resource.name' => Inflector::singularize($resourceName),
+			),
+			'contain' => array(
+				'Role' => array(
+					'conditions' => array('Role.id' => $myRoleIds)
+				),
+				'Resource'
+			),
+			'fields' => array('Permission.name'),
+		));
+
+		foreach ($permissions as $p => $permission) {
+			if (!empty($permission['Role'])) {
+				$permissionNames[] = $permission['Permission']['name'];
+			}
+		}
+		if (is_array($permissionNames)) {
+			foreach ($permissionNames as $name) {
+				$methods = $this->permissionsMap[$name];
+				foreach ($methods as $method) {
+					$permittedMethods[] = $method;
+				}
+			}
+			return $permittedMethods;
+		}
+	}
 }
